d8e294e64993d744b59ac0be6e1ab6bae0caed1c
fasaic <fasaic.113@gmail.com>
2022-08-26 12:57:49 +0100

    heroku deploy

--- server.js
+++ server.js
@@ -5,22 +5,24 @@ import router from './router.js'
 import CONSTS from './consts.js'
 import cors from 'cors'
 import errorHandler from './middleware/errorHandler.js'
-
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-app.use(logger)
-app.use(router)
-app.use(errorHandler)
-
-// ! CATCH
-app.use((req, res, next) => {
-  console.log('request cant be met', req.url, req.method)
-  return res.status(404).send('404 - Required endpoint not found.')
-})
+import dotenv from 'dotenv'
 
 const startServer = async () => {
+  const app = express()
+  dotenv.config()
+  
+  app.use(cors())
+  app.use(express.json())
+  app.use(logger)
+  app.use(router)
+  app.use(errorHandler)
+
+  // ! CATCH
+  app.use((req, res, next) => {
+    console.log('request cant be met', req.url, req.method)
+    return res.status(404).send('404 - Required endpoint not found.')
+  })
+
   await connectToDb()
   console.log('Database connected')
   app.listen(CONSTS.PORT, () => {
